83d36e4359cea628fc5c9762dd5932c580365252
mis25 <mis2560@naver.com>
2022-12-14 16:29:06 +0900

    가계부 개인 프로젝트 help폴더 업데이트

---
+++ cashbook/src/main/java/vo/Help.java
@@ -0,0 +1,40 @@
+package vo;
+
+public class Help {
+	private int HelpNo;
+	private String HelpMemo;
+	private String memberId;
+	private String updatedate;
+	private String createdate;
+	public int getHelpNo() {
+		return HelpNo;
+	}
+	public void setHelpNo(int helpNo) {
+		HelpNo = helpNo;
+	}
+	public String getHelpMemo() {
+		return HelpMemo;
+	}
+	public void setHelpMemo(String helpMemo) {
+		HelpMemo = helpMemo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+		
+}
